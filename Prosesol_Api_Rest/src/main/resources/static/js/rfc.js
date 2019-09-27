b66caa188fa04cfd81845cd51db5e7e5d6a0b3eb
alexander <alexander@LAPTOP-SRFTDOHR>
2019-09-27 13:43:06 -0500

    se actualizo las alertas ajax

--- Prosesol_Api_Rest/src/main/resources/static/js/rfc.js
+++ Prosesol_Api_Rest/src/main/resources/static/js/rfc.js
@@ -10,43 +10,54 @@ function generar() {
 
     if(!$("#nombre").val()){
         if($("#nombre").parent().next(".validation").length == 0){
-            $("#nombre").parent().after("<div class='validation' style='color:red;margin-bottom: 20px;'>Campo obligatorio</div>")
+           // $("#nombre").parent().after("<div class='validation' style='color:red;margin-bottom: 20px;'>Campo obligatorio</div>")
+        	 $("#validaNombre").delay(100).fadeIn("slow"); 
         }
         $("#nombre").focus();
         focusSet = true;
         validation = false;
     }else{
-        $("#nombre").parent().next(".validation").remove();
+        //$("#nombre").parent().next(".validation").remove();
+    	 $("#validaNombre").fadeOut("slow");
+    	
     }
     if(!$("#apellidoPaterno").val()){
         if($("#apellidoPaterno").parent().next(".validation").length == 0){
-            $("#apellidoPaterno").parent().after("<div class='validation' style='color:red;margin-bottom: 20px;'>Campo obligatorio</div>")
+           // $("#apellidoPaterno").parent().after("<div class='validation' style='color:red;margin-bottom: 20px;'>Campo obligatorio</div>")
+        	 $("#validaApellidoPaterno").delay(100).fadeIn("slow"); 
+
         }
         $("#apellidoPaterno").focus();
+        
         focusSet = true;
         validation = false;
     }else{
-        $("#apellidoPaterno").parent().next(".validation").remove();
+       // $("#apellidoPaterno").parent().next(".validation").remove();
+        $("#validaApellidoPaterno").fadeOut("slow");
     }
     if(!$("#apellidoMaterno").val()){
         if($("#apellidoMaterno").parent().next(".validation").length == 0){
-            $("#apellidoMaterno").parent().after("<div class='validation' style='color:red;margin-bottom: 20px;'>Campo obligatorio</div>")
+            //$("#apellidoMaterno").parent().after("<div class='validation' style='color:red;margin-bottom: 20px;'>Campo obligatorio</div>")
+        	 $("#validaApellidoMaterno").delay(100).fadeIn("slow");
         }
         $("#apellidoMaterno").focus();
         focusSet = true;
         validation = false;
     }else{
-        $("#apellidoMaterno").parent().next(".validation").remove();
+       // $("#apellidoMaterno").parent().next(".validation").remove();
+    	 $("#validaApellidoMaterno").fadeOut("slow");
     }
      if(!$("#fechaNacimiento").val()){
          if($("#fechaNacimiento").parent().next(".validation").length == 0){
-             $("#fechaNacimiento").parent().after("<div class='validation' style='color:red;margin-bottom: 20px;'>Campo obligatorio</div>")
+            // $("#fechaNacimiento").parent().after("<div class='validation' style='color:red;margin-bottom: 20px;'>Campo obligatorio</div>")
+        	 $("#validaFechaNacimiento").delay(100).fadeIn("slow");
          }
          $("#fechaNacimiento").focus();
          focusSet = true;
          validation = false;
      }else{
-        $("#fechaNacimiento").parent().next(".validation").remove();
+        //$("#fechaNacimiento").parent().next(".validation").remove();
+    	 $("#validaFechaNacimiento").fadeOut("slow");
      }
 
      if(validation == true){
