f4a25dc854d58982961d67111c12ce41ee452f41
alexander <alexander@LAPTOP-SRFTDOHR>
2020-03-05 11:20:19 -0600

    creacion de un page de errores

--- Prosesol_Api_Rest/src/main/java/com/prosesol/api/rest/auth/security/APISecurityConfig.java
+++ Prosesol_Api_Rest/src/main/java/com/prosesol/api/rest/auth/security/APISecurityConfig.java
@@ -30,7 +30,7 @@ public class APISecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/", "/css/**", "/js/**", "/pagos/**", "/afiliados/**"
-				,"/beneficiarios/**", "/index", "/obtener/**", "/webhook/**")
+				,"/beneficiarios/**", "/index", "/obtener/**", "/webhook/**","/error")
 			.permitAll()
 			.anyRequest()
 			.authenticated()
